50728a1aad9c711ac0858394cf7fda50fe1a0403
theflexone <edhavey@gmail.com>
2023-03-26 22:32:56 -0500

    FIRST!

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="h-screen grid items-center text-center">
+      <h2 className="">Hello WOrld!</h2>
+    </div>
+  );
+}
+
+export default App;
